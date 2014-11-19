59266b5ddf7db6b9b80e9211fe9d93b1a972bedf
Jamie Humphries <jamie.humphries@softwire.com>
2014-11-19 15:17:42 +0000

    Add WPF key mappings.

--- Cheet.Wpf.Tests/CheetTests.cs
+++ Cheet.Wpf.Tests/CheetTests.cs
@@ -1,7 +1,154 @@
 ﻿namespace Cheet.Wpf.Tests
 {
+    using FluentAssertions;
     using NUnit.Framework;
+    using System;
+    using System.Collections;
+    using System.Windows.Input;
 
     [TestFixture]
-    public class CheetTests {}
+    public class CheetTests
+    {
+        private TestCheet cheet;
+
+        [SetUp]
+        public void SetUp()
+        {
+            cheet = new TestCheet();
+        }
+
+        [TestCaseSource("KeyNameMappings")]
+        public void All_key_names_mapped_correctly(string keyName, Key expectedKey)
+        {
+            cheet.GetKey(keyName).Should().Be(expectedKey);
+        }
+
+        [Test]
+        public void Exception_is_thrown_if_key_name_cannot_be_mapped()
+        {
+            // When
+            Action mappingBadKeyName = () => cheet.GetKey("bad_name");
+
+            // Then
+            mappingBadKeyName.ShouldThrow<ArgumentException>().WithMessage("Could not map key named 'bad_name'.");
+        }
+
+        public IEnumerable KeyNameMappings()
+        {
+            yield return new TestCaseData("left", Key.Left);
+            yield return new TestCaseData("L", Key.Left);
+            yield return new TestCaseData("←", Key.Left);
+            yield return new TestCaseData("up", Key.Up);
+            yield return new TestCaseData("U", Key.Up);
+            yield return new TestCaseData("↑", Key.Up);
+            yield return new TestCaseData("right", Key.Right);
+            yield return new TestCaseData("R", Key.Right);
+            yield return new TestCaseData("→", Key.Right);
+            yield return new TestCaseData("down", Key.Down);
+            yield return new TestCaseData("D", Key.Down);
+            yield return new TestCaseData("↓", Key.Down);
+            yield return new TestCaseData("0", Key.D0);
+            yield return new TestCaseData("1", Key.D1);
+            yield return new TestCaseData("2", Key.D2);
+            yield return new TestCaseData("3", Key.D3);
+            yield return new TestCaseData("4", Key.D4);
+            yield return new TestCaseData("5", Key.D5);
+            yield return new TestCaseData("6", Key.D6);
+            yield return new TestCaseData("7", Key.D7);
+            yield return new TestCaseData("8", Key.D8);
+            yield return new TestCaseData("9", Key.D9);
+            yield return new TestCaseData("a", Key.A);
+            yield return new TestCaseData("b", Key.B);
+            yield return new TestCaseData("c", Key.C);
+            yield return new TestCaseData("d", Key.D);
+            yield return new TestCaseData("e", Key.E);
+            yield return new TestCaseData("f", Key.F);
+            yield return new TestCaseData("g", Key.G);
+            yield return new TestCaseData("h", Key.H);
+            yield return new TestCaseData("i", Key.I);
+            yield return new TestCaseData("j", Key.J);
+            yield return new TestCaseData("k", Key.K);
+            yield return new TestCaseData("l", Key.L);
+            yield return new TestCaseData("m", Key.M);
+            yield return new TestCaseData("n", Key.N);
+            yield return new TestCaseData("o", Key.O);
+            yield return new TestCaseData("p", Key.P);
+            yield return new TestCaseData("q", Key.Q);
+            yield return new TestCaseData("r", Key.R);
+            yield return new TestCaseData("s", Key.S);
+            yield return new TestCaseData("t", Key.T);
+            yield return new TestCaseData("u", Key.U);
+            yield return new TestCaseData("v", Key.V);
+            yield return new TestCaseData("w", Key.W);
+            yield return new TestCaseData("x", Key.X);
+            yield return new TestCaseData("y", Key.Y);
+            yield return new TestCaseData("z", Key.Z);
+            yield return new TestCaseData("backspace", Key.Back);
+            yield return new TestCaseData("tab", Key.Tab);
+            yield return new TestCaseData("enter", Key.Enter);
+            yield return new TestCaseData("return", Key.Return);
+            yield return new TestCaseData("shift", Key.LeftShift);
+            yield return new TestCaseData("⇧", Key.LeftShift);
+            yield return new TestCaseData("control", Key.LeftCtrl);
+            yield return new TestCaseData("ctrl", Key.LeftCtrl);
+            yield return new TestCaseData("^", Key.LeftCtrl);
+            yield return new TestCaseData("alt", Key.LeftAlt);
+            yield return new TestCaseData("option", Key.LeftAlt);
+            yield return new TestCaseData("⌥", Key.LeftAlt);
+            yield return new TestCaseData("command", Key.LWin);
+            yield return new TestCaseData("⌘", Key.LWin);
+            yield return new TestCaseData("pause", Key.Pause);
+            yield return new TestCaseData("capslock", Key.CapsLock);
+            yield return new TestCaseData("esc", Key.Escape);
+            yield return new TestCaseData("space", Key.Space);
+            yield return new TestCaseData("pageup", Key.PageUp);
+            yield return new TestCaseData("pagedown", Key.PageDown);
+            yield return new TestCaseData("end", Key.End);
+            yield return new TestCaseData("home", Key.Home);
+            yield return new TestCaseData("insert", Key.Insert);
+            yield return new TestCaseData("delete", Key.Delete);
+            yield return new TestCaseData("equal", Key.OemPlus);
+            yield return new TestCaseData("comma", Key.OemComma);
+            yield return new TestCaseData(",", Key.OemComma);
+            yield return new TestCaseData("minus", Key.OemMinus);
+            yield return new TestCaseData("-", Key.OemMinus);
+            yield return new TestCaseData("period", Key.OemPeriod);
+            yield return new TestCaseData(".", Key.OemPeriod);
+            yield return new TestCaseData("kp_0", Key.NumPad0);
+            yield return new TestCaseData("kp_1", Key.NumPad1);
+            yield return new TestCaseData("kp_2", Key.NumPad2);
+            yield return new TestCaseData("kp_3", Key.NumPad3);
+            yield return new TestCaseData("kp_4", Key.NumPad4);
+            yield return new TestCaseData("kp_5", Key.NumPad5);
+            yield return new TestCaseData("kp_6", Key.NumPad6);
+            yield return new TestCaseData("kp_7", Key.NumPad7);
+            yield return new TestCaseData("kp_8", Key.NumPad8);
+            yield return new TestCaseData("kp_9", Key.NumPad9);
+            yield return new TestCaseData("kp_multiply", Key.Multiply);
+            yield return new TestCaseData("kp_plus", Key.Add);
+            yield return new TestCaseData("kp_minus", Key.Subtract);
+            yield return new TestCaseData("kp_decimal", Key.Decimal);
+            yield return new TestCaseData("kp_divide", Key.Divide);
+            yield return new TestCaseData("f1", Key.F1);
+            yield return new TestCaseData("f2", Key.F2);
+            yield return new TestCaseData("f3", Key.F3);
+            yield return new TestCaseData("f4", Key.F4);
+            yield return new TestCaseData("f5", Key.F5);
+            yield return new TestCaseData("f6", Key.F6);
+            yield return new TestCaseData("f7", Key.F7);
+            yield return new TestCaseData("f8", Key.F8);
+            yield return new TestCaseData("f9", Key.F9);
+            yield return new TestCaseData("f10", Key.F10);
+            yield return new TestCaseData("f11", Key.F11);
+            yield return new TestCaseData("f12", Key.F12);
+        }
+    }
+
+    internal class TestCheet : Cheet
+    {
+        public new Key GetKey(string keyName)
+        {
+            return base.GetKey(keyName);
+        }
+    }
 }
--- Cheet.Wpf/Cheet.cs
+++ Cheet.Wpf/Cheet.cs
@@ -1,13 +1,124 @@
 ﻿namespace Cheet.Wpf
 {
     using global::Cheet.Core;
+    using System;
+    using System.Text.RegularExpressions;
     using System.Windows.Input;
 
     public class Cheet : Cheet<Key>
     {
+        private static readonly Regex LetterKeyNamePattern = new Regex(@"^[a-z]$");
+        private static readonly Regex NumberKeyNamePattern = new Regex(@"^[0-9]$");
+        private static readonly Regex KeypadNumberKeyNamePattern = new Regex(@"^kp_[0-9]$");
+        private static readonly Regex FunctionKeyNamePattern = new Regex(@"^(?:f[1-9]|f1[0-2])$");
+
         protected override Key GetKey(string keyName)
         {
-            return Key.A;
+            if (LetterKeyNamePattern.IsMatch(keyName))
+            {
+                return ParseKey(keyName.ToUpper());
+            }
+            if (NumberKeyNamePattern.IsMatch(keyName))
+            {
+                return ParseKey("D" + keyName);
+            }
+            if (KeypadNumberKeyNamePattern.IsMatch(keyName))
+            {
+                return ParseKey(keyName.Replace("kp_", "NumPad"));
+            }
+            if (FunctionKeyNamePattern.IsMatch(keyName))
+            {
+                return ParseKey(keyName.ToUpper());
+            }
+            switch (keyName)
+            {
+                case "left":
+                case "L":
+                case "←":
+                    return Key.Left;
+                case "up":
+                case "U":
+                case "↑":
+                    return Key.Up;
+                case "right":
+                case "R":
+                case "→":
+                    return Key.Right;
+                case "down":
+                case "D":
+                case "↓":
+                    return Key.Down;
+                case "backspace":
+                    return Key.Back;
+                case "tab":
+                    return Key.Tab;
+                case "enter":
+                    return Key.Enter;
+                case "return":
+                    return Key.Return;
+                case "shift":
+                case "⇧":
+                    return Key.LeftShift;
+                case "control":
+                case "ctrl":
+                case "^":
+                    return Key.LeftCtrl;
+                case "alt":
+                case "option":
+                case "⌥":
+                    return Key.LeftAlt;
+                case "command":
+                case "⌘":
+                    return Key.LWin;
+                case "pause":
+                    return Key.Pause;
+                case "capslock":
+                    return Key.CapsLock;
+                case "esc":
+                    return Key.Escape;
+                case "space":
+                    return Key.Space;
+                case "pageup":
+                    return Key.PageUp;
+                case "pagedown":
+                    return Key.PageDown;
+                case "end":
+                    return Key.End;
+                case "home":
+                    return Key.Home;
+                case "insert":
+                    return Key.Insert;
+                case "delete":
+                    return Key.Delete;
+                case "equal":
+                case "=":
+                    return Key.OemPlus;
+                case "comma":
+                case ",":
+                    return Key.OemComma;
+                case "minus":
+                case "-":
+                    return Key.OemMinus;
+                case "period":
+                case ".":
+                    return Key.OemPeriod;
+                case "kp_multiply":
+                    return Key.Multiply;
+                case "kp_plus":
+                    return Key.Add;
+                case "kp_minus":
+                    return Key.Subtract;
+                case "kp_decimal":
+                    return Key.Decimal;
+                case "kp_divide":
+                    return Key.Divide;
+            }
+            throw new ArgumentException(String.Format("Could not map key named '{0}'.", keyName));
+        }
+
+        private static Key ParseKey(string keyName)
+        {
+            return (Key)Enum.Parse(typeof(Key), keyName);
         }
     }
 }
