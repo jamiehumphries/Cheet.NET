9b6c403f68da6acdb8104ee22539fc6268a6f73c
Jamie Humphries <jamie.humphries@softwire.com>
2014-11-20 10:14:57 +0000

    Reindent example to match README.md.

--- Cheet.Wpf.Demo/MainWindow.xaml.cs
+++ Cheet.Wpf.Demo/MainWindow.xaml.cs
@@ -10,45 +10,44 @@
             var cheet = new Cheet();
             PreviewKeyDown += cheet.OnKeyDown;
 
-            cheet.Map("↑ ↑ ↓ ↓ ← → ← → b a", () => WriteLine("Voilà!"));
+            cheet.Map("↑ ↑ ↓ ↓ ← → ← → b a", () => { WriteLine("Voilà!"); });
 
-            cheet.Map("i d d q d", () => WriteLine("god mode enabled"));
+            cheet.Map("i d d q d", () => {
+                WriteLine("god mode enabled");
+            });
 
-            cheet.Map("o n e a t a t i m e", new CheetCallbacks
-                                             {
-                                                 Next = (str, key, num, seq) =>
-                                                 {
-                                                     WriteLine("key pressed: " + key);
-                                                     WriteLine("progress: " + (double)num / seq.Length);
-                                                     WriteLine("seq: " + String.Join(" ", seq));
-                                                 },
-                                                 Fail = (str, seq) => WriteLine("sequence failed"),
-                                                 Done = (str, seq) => WriteLine("+30 lives ;)")
-                                             });
+            cheet.Map("o n e a t a t i m e", new CheetCallbacks {
+                Next = (str, key, num, seq) => {
+                    WriteLine("key pressed: " + key);
+                    WriteLine("progress: " + (double)num / seq.Length);
+                    WriteLine("seq: " + String.Join(" ", seq));
+                },
+                Fail = (str, seq) => {
+                    WriteLine("sequence failed");
+                },
+                Done = (str, seq) => {
+                    WriteLine("+30 lives ;)");
+                }
+            });
 
-            cheet.Map("o n c e", () =>
-            {
+            cheet.Map("o n c e", () => {
                 WriteLine("This will only fire once.");
                 cheet.Disable("o n c e");
             });
 
-            dynamic sequences = new
-                                {
-                                    Cross = "up down left right",
-                                    Circle = "left up right down"
-                                };
+            dynamic sequences = new {
+                Cross = "up down left right",
+                Circle = "left up right down"
+            };
 
             cheet.Map(sequences.Cross);
             cheet.Map(sequences.Circle);
 
-            cheet.Done((str, seq) =>
-            {
-                if (str == sequences.Cross)
-                {
+            cheet.Done((str, seq) => {
+                if (str == sequences.Cross) {
                     WriteLine("cross!");
                 }
-                else if (str == sequences.Circle)
-                {
+                else if (str == sequences.Circle) {
                     WriteLine("circle!");
                 }
             });
