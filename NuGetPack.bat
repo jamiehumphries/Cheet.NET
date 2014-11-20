b787df1261ce97bdd8e2c8c83f3852fff1688b53
Jamie Humphries <jamie.humphries@softwire.com>
2014-11-20 10:26:45 +0000

    Split project in to two NuGet packages.

--- NuGetPack.bat
+++ NuGetPack.bat
@@ -1,8 +1,13 @@
 @echo off
 set /p version="Version: "
 msbuild Cheet.sln /P:Configuration=Release
-rmdir /S /Q nuget-pack\lib
-xcopy Cheet.Wpf\bin\Release\Cheet.Wpf.dll nuget-pack\lib\4.5\ /Y
-xcopy Cheet.Wpf\bin\Release\Cheet.Core.dll nuget-pack\lib\4.5\ /Y
-.nuget\nuget pack nuget-pack\Cheet.Wpf.nuspec -Version %version%
+
+rmdir /S /Q nuget-pack\Cheet.Core\lib
+xcopy Cheet.Core\bin\Release\Cheet.Core.dll nuget-pack\Cheet.Core\lib\4.5\ /Y
+.nuget\nuget pack nuget-pack\Cheet.Core\Cheet.Core.nuspec -Version %version%
+
+rmdir /S /Q nuget-pack\Cheet.Wpf\lib
+xcopy Cheet.Wpf\bin\Release\Cheet.Wpf.dll nuget-pack\Cheet.Wpf\lib\4.5\ /Y
+.nuget\nuget pack nuget-pack\Cheet.Wpf\Cheet.Wpf.nuspec -Version %version%
+
 pause
